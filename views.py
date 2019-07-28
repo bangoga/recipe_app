2d7278b563efb8c9b446507128b1b3a078db74d4
Khalil Mohsin <mohsin.khalil8@gmail.com>
2019-07-28 09:31:15 -0400

    Adding Info Handling
    + Added functions in view and template to handle info

--- views.py
+++ views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        items = request.POST.get("fullValues","")
+        print("\n\n")
+        print(items)
+        
     return render(request,"foodsrch/index.html")
+
+
+
+def handle_item_request(items):
+    print("handling")
+
+
+"A function that is used by handle_item to decrypt the original item request"
+def decode_item_request(item_request):
+    print("decoding")
