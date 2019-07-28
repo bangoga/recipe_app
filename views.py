c4ba0ee5ced504b2c778d053b060bdb68b6dcc8f
Khalil Mohsin <mohsin.khalil8@gmail.com>
2019-07-28 11:55:35 -0400

    Bug Fixes
    Minor Bug Fixes

--- views.py
+++ views.py
@@ -6,17 +6,20 @@ from django.shortcuts import render
 def index(request):
     if request.method == "POST":
         items = request.POST.get("fullValues","")
-        print("\n\n")
-        print(items)
-        
+        print("\n")
+        item_list = decode_item_request(items)
+        handle_item_request(item_list)
+
     return render(request,"foodsrch/index.html")
 
 
 
 def handle_item_request(items):
-    print("handling")
-
+    print("Post Handling")
+    
 
 "A function that is used by handle_item to decrypt the original item request"
 def decode_item_request(item_request):
-    print("decoding")
+    all_items = item_request.split("-")
+    all_items.remove("")
+    return all_items
