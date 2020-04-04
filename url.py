6bf2023a02db0421797e6775a2b2325bb47e3f8c
Khalil Mohsin <mohsin.khalil8@gmail.com>
2020-04-04 12:42:44 -0400

    Change in UI Major

--- url.py
+++ url.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
+from django.conf.urls import url
 
+"This is the main url setting for the current app, if you are navigating no where, you show views.index"
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('foodsrch/search_result_list',views.result,name="search_result_list"),
+    path('foodsrch/search_result_list',views.index,name="search_result_list")
 ]
--- views.py
+++ views.py
@@ -1,25 +1,61 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from bs4 import BeautifulSoup
+import urllib.request
+from selenium import webdriver
+import os
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
         items = request.POST.get("fullValues","")
         print("\n")
-        item_list = decode_item_request(items)
-        handle_item_request(item_list)
+        # item_list = decode_item_request(items)
+        # handle_item_request(item_list)
+        return render(request,"foodsrch/search_result_list.html",{'item':item_list})
 
-    return render(request,"foodsrch/index.html")
+    else:
+        spiceList = readTxt()
+        return render(request,"foodsrch/index.html",{'spices':spiceList})
+
+
+
+#----------------------------------- [Reads all the possible spices from the local txt file]-------------------------------#
+def readTxt():
+    module_dir = os.path.dirname(__file__)
+    file_path = os.path.join(module_dir, 'spice.txt')   # Reads line by line and saves them to data[]
+    with open(file_path , 'r') as f:
+        data = [line.strip() for line in f]
+    print(data[1])
+
+    return data
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 def handle_item_request(items):
+    print(items)
     print("Post Handling")
-    
+
 
 "A function that is used by handle_item to decrypt the original item request"
 def decode_item_request(item_request):
     all_items = item_request.split("-")
     all_items.remove("")
     return all_items
+
+def result(request):
+    return render(request,"foodsrch/search_result_list.html",{'post':'Why is this working or is it really working? '})
+
+"Current its being shown as such but what if you what is class rendering?"
